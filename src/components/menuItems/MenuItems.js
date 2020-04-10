6f46120ec57cdb1431aae12a1c5d9467e684b2ee
Sotiris Tsiamouris <tsot85@gmail.com>
2020-04-10 13:55:19 +0300

    to-do menu items

--- src/components/menuItems/MenuItems.js
+++ src/components/menuItems/MenuItems.js
@@ -11,8 +11,12 @@ const MenuItems = React.forwardRef(({ handleClose, ...restProps }, ref) => (
     >
       Using Styles HOC
     </MenuItem>
-    <MenuItem onClick={handleClose}>Example 1</MenuItem>
-    <MenuItem onClick={handleClose}>Example 2</MenuItem>
+    <MenuItem onClick={handleClose}>(TO-DO) Creating a Card</MenuItem>
+    <MenuItem onClick={handleClose}>(TO-DO) Cards in FlexBox</MenuItem>
+    <MenuItem onClick={handleClose}>(TO-DO) Cards in CSS-Grid</MenuItem>
+    <MenuItem onClick={handleClose}>(TO-DO) Creating a DashBoard</MenuItem>
+    <MenuItem onClick={handleClose}>(TO-DO) Using Recharts</MenuItem>
+    <MenuItem onClick={handleClose}>(TO-DO) Recharts in DashBoard</MenuItem>
   </>
 ));
 
